c356285ac9ef73d684ae5740e6f0338249f6ec15
Jeric Epon <jericepon@gmail.com>
2020-05-18 11:35:54 +0800

    Templates & Inheritance | Django Framework (3.0) Crash Course Tutorials (pt 3)

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'accounts/dashboard.html')
+
+
+def products(request):
+    return render(request, 'accounts/products.html')
+
+
+def customer(request):
+    return render(request, 'accounts/customer.html')
