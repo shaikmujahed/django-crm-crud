704809ba3aa4be11b48ab3c68e9290374936e183
Jeric Epon <jericepon@gmail.com>
2020-05-24 19:13:18 +0800

    Database Relationships | One To Many & Many to Many | Django (3.0) Crash Course Tutorials (pt 6)

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Customer, Product, Order
+from .models import Customer, Product, Order, Tag
 # Register your models here.
 
 class CustomerAdmin(admin.ModelAdmin):
@@ -11,6 +11,11 @@ class ProductAdmin(admin.ModelAdmin):
 class OrderAdmin(admin.ModelAdmin):
   list_display = ['status', 'created_at']
   
+  
+class TagAdmin(admin.ModelAdmin):
+  list_display = ['name']
+  
 admin.site.register(Customer, CustomerAdmin)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Order, OrderAdmin)
+admin.site.register(Tag, TagAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -13,6 +13,13 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORIES = (
         ('Indoor', 'Indoor'),
@@ -21,8 +28,12 @@ class Product(models.Model):
     name = models.CharField(max_length=255, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=255, null=True, choices=CATEGORIES)
-    description = models.CharField(max_length=255, null=True)
+    description = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -31,7 +42,11 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivered', 'Delivered'),
     )
-    # customer
-    # product
+    customer = models.ForeignKey(
+        Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     status = models.CharField(max_length=255, null=True, choices=STATUS)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
