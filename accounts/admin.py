4c9384d234bc71ea761574417bec2abb60563232
Jeric Epon <jericepon@gmail.com>
2020-05-24 18:30:13 +0800

    Database Models & Admin Panel | Django Framework (3.0) Crash Course Tutorials (pt 5)

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Customer, Product, Order
 # Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+  list_display = ['name', 'email', 'phone']
+  
+class ProductAdmin(admin.ModelAdmin):
+  list_display = ['name', 'price', 'created_at']
+  
+class OrderAdmin(admin.ModelAdmin):
+  list_display = ['status', 'created_at']
+  
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    phone = models.CharField(max_length=255, null=True)
+    email = models.CharField(max_length=255, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORIES = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+    name = models.CharField(max_length=255, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=255, null=True, choices=CATEGORIES)
+    description = models.CharField(max_length=255, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
+    )
+    # customer
+    # product
+    status = models.CharField(max_length=255, null=True, choices=STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
