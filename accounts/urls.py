be012ce4d78c874b4f01b17f122bd04840c46f8b
Jeric Epon <jericepon@gmail.com>
2020-05-25 13:09:00 +0800

    Dynamic URL's (Part 2) | Django (3.0) Crash Course Tutorials (pt 9.5)

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index),
-  path('products/', views.products),
-  path('customer/<str:id>/', views.customer),
+  path('', views.index, name='index'),
+  path('products/', views.products, name='products'),
+  path('customer/<str:id>/', views.customer, name='customer'),
 ]
