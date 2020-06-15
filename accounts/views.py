422762b5c449c6446ea11c12f3eb2a0956896a14
jericepon <jeric.epon@stokmanila.com>
2020-06-15 18:09:38 +0800

    User Profile With One To One Relationship | Django (3.0) Crash Course Tutorials (pt 16)

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render, redirect
+# Core
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+# Project file imports
 from accounts.models import *
 from .forms import OrderForm, RegistrationForm
 from .filters import OrderFilter
+from .decorators import *
+
 
 @login_required(login_url='login')
+@admin_only
 def index(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -17,32 +22,31 @@ def index(request):
     delivered = orders.filter(status="Delivered").count()
     pending = orders.filter(status="Pending").count()
 
-    return render(request, 'accounts/dashboard.html', {
-        'orders': orders,
-        'customers': customers,
-        'total_customers': total_customers,
-        'total_orders': total_orders,
-        'delivered': delivered,
-        'pending': pending
-    })
+    return render(
+        request, 'accounts/dashboard.html', {
+            'orders': orders,
+            'customers': customers,
+            'total_customers': total_customers,
+            'total_orders': total_orders,
+            'delivered': delivered,
+            'pending': pending
+        })
+
 
 def products(request):
     products = Product.objects.all()
-    return render(request, 'accounts/products.html', {
-        'products': products
-    })
+    return render(request, 'accounts/products.html', {'products': products})
+
 
 @login_required(login_url='login')
-def customer(request, id):
-    customer = Customer.objects.get(id=id)
-
+def customer(request, pk):
+    customer = Customer.objects.get(pk=pk)
     orders = customer.order_set.all()
     order_count = orders.count()
 
     order_filter = OrderFilter(request.GET, queryset=orders)
     orders = order_filter.qs
 
-    print(request.GET, orders)
     context = {
         'customer': customer,
         'orders': orders,
@@ -50,6 +54,7 @@ def customer(request, id):
         'order_filter': order_filter
     }
     return render(request, 'accounts/customer.html', context)
+
 
 @login_required(login_url='login')
 def create_order(request):
@@ -63,6 +68,7 @@ def create_order(request):
 
     context = {'form': form}
     return render(request, 'accounts/create_order_form.html', context)
+
 
 @login_required(login_url='login')
 def update_order(request, pk):
@@ -78,6 +84,7 @@ def update_order(request, pk):
     context = {'form': form}
     return render(request, 'accounts/update_order_form.html', context)
 
+
 @login_required(login_url='login')
 def delete_order(request, pk):
     order = Order.objects.get(pk=pk)
@@ -90,39 +97,36 @@ def delete_order(request, pk):
     return render(request, 'accounts/delete_order.html', context)
 
 
+@unauthorized_user
 def loginView(request):
-    if request.user.is_authenticated:
-        return redirect('index')
-    else:
-        if request.method == 'POST':
+    if request.method == 'POST':
 
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(request, username=username, password=password)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
 
-            if user is not None:
-                login(request, user)
-                return redirect('index')
-            else:
-                messages.info(request, 'Username and Password is incorrect.')
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Username and Password is incorrect.')
 
-        return render(request, 'accounts/login.html')
+    return render(request, 'accounts/login.html')
 
 
+@unauthorized_user
 def register(request):
-    if request.user.is_authenticated:
-        return redirect('index')
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(
+                request, f'Account has  been successfully created for {user}')
+            return redirect('login')
     else:
         form = RegistrationForm()
-        if request.method == 'POST':
-            form = RegistrationForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, f'Account has  been successfully created for {user}')
-                return redirect('login')
-        else:
-            form = RegistrationForm()
 
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
